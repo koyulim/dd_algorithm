353f9e1af5d930b5f12bcd4a48067e0591d29f5c
고유림 <89690794+koyulim@users.noreply.github.com>
2025-03-21 11:03:45 +0900

    Bigger than you

--- recursive_algorithm/1. Bigger than you/Main.java
+++ recursive_algorithm/1. Bigger than you/Main.java
@@ -1,3 +1,38 @@
+import java.io.*;
 public class Main {
-    
+	
+	private static int n;
+	private static long k, result;
+	private static int[] list;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		n = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().split(" ");
+		list = new int[n];
+		for (int i = 0; i < n; i++) {
+			list[i] = Integer.parseInt(input[i]);
+		}
+		k = Long.parseLong(br.readLine());
+
+		result = Long.MAX_VALUE;
+		solve(0);
+		
+		bw.write(result + "\n");
+		bw.flush();
+	}
+	
+	private static void solve(long v) {
+		if (v > k) {
+			result = Math.min(result, v);
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			if (list[i] == 0 && v == 0) continue;
+			solve(v * 10 + list[i]);
+		}
+	}
 }
