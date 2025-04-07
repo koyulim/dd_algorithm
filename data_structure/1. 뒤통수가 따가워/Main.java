c007a8c46af0f88a87b56b0a8e309a3384a89f45
koyulim <89690794+koyulim@users.noreply.github.com>
2025-04-07 10:40:51 +0900

    Update Main.java

--- data_structure/1. 뒤통수가 따가워/Main.java
+++ data_structure/1. 뒤통수가 따가워/Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-class Main {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
