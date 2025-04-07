4631c3ab7e6d17225a2259753bbb4c4f06c92377
yulim <kul2862@naver.com>
2025-04-07 10:37:59 +0900

    data_structure

---
+++ data_structure/1. 뒤통수가 따가워/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().split(" ");
+		
+		int max = 0;
+		int[] list = new int[n];
+		for (int i = 0; i < n; i++) {
+			list[i] = Integer.parseInt(input[i]);
+			max = Math.max(max, list[i]);
+		}
+		
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < n; i++) {
+			bw.write(stack.size() + " ");
+			
+			while (!stack.empty() && stack.peek() <= list[i]) {
+				stack.pop();
+			}
+			stack.add(list[i]);
+		}
+		
+		bw.write("\n");
+		bw.flush();
+	}
+}
